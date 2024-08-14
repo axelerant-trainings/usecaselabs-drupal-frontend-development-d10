ad9ba8be19e4ed4ff5e63cf6560eca9db07fbb21
Mudasir Ali <mudasir.ali@axelerant.com>
2024-08-14 16:21:57 +0530

    Added mega-menu

--- web/themes/custom/usecaselabs/dist/js/global.js
+++ web/themes/custom/usecaselabs/dist/js/global.js
@@ -9,6 +9,19 @@
   Drupal.behaviors.mobile_menu = {
     attach: function attach(context, settings) {
       // console.log('test');
+
+      document.querySelectorAll('.menu--level-1 > li > a').forEach(function (menuLink) {
+        menuLink.addEventListener('keydown', function (event) {
+          // Handle spacebar (key code 32)
+          if (event.keyCode === 32) {
+            event.preventDefault(); // Prevent the default spacebar action (scrolling)
+            var submenu = this.nextElementSibling;
+            if (submenu && submenu.classList.contains('menu--level-2')) {
+              submenu.style.display = submenu.style.display === 'flex' ? 'none' : 'flex';
+            }
+          }
+        });
+      });
     }
   };
 })(Drupal, drupalSettings);
--- web/themes/custom/usecaselabs/js/global.js
+++ web/themes/custom/usecaselabs/js/global.js
@@ -7,6 +7,19 @@
   Drupal.behaviors.mobile_menu = {
     attach: function (context, settings) {
       // console.log('test');
+
+      document.querySelectorAll('.menu--level-1 > li > a').forEach(menuLink => {
+        menuLink.addEventListener('keydown', function(event) {
+            // Handle spacebar (key code 32)
+            if (event.keyCode === 32) {
+                event.preventDefault(); // Prevent the default spacebar action (scrolling)
+                const submenu = this.nextElementSibling;
+                if (submenu && submenu.classList.contains('menu--level-2')) {
+                    submenu.style.display = submenu.style.display === 'flex' ? 'none' : 'flex';
+                }
+            }
+        });
+    });
     },
   };
 })(Drupal, drupalSettings);
