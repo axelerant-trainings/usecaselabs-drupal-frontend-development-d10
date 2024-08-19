e0801f64a2267ac179bff9d6926b7d344e74c4f6
Mudasir Ali <mudasir.ali@axelerant.com>
2024-08-19 20:19:51 +0530

    LDT-737: enabling slick slider

---
+++ web/themes/custom/usecaselabs/dist/js/components/slick-slider.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/**
+ * @file
+ * script.js
+ */
+
+(function (Drupal, $) {
+  Drupal.behaviors.slick = {
+    attach: function attach(context, settings) {
+      $('.slick-slider').slick({
+        accessibility: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        variableWidth: false,
+        autoplay: true,
+        dots: true,
+        responsive: [{
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 1,
+            prevArrow: '.slick-slider-container .prev-btn',
+            nextArrow: '.slick-slider-container .next-btn'
+          }
+        }]
+      });
+    }
+  };
+})(Drupal, jQuery);
